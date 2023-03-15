a339865a0f479689d5109ec9771eeedcfc07da90
Build Er <builder@macmini6.local>
2023-03-15 15:37:06 -0300

    Package version: 1.0.0-beta+20230315182831

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXGAM",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXGAM-1.0.0-beta+20230315133647.xcframework.zip",
-			checksum: "9ab5485b4a894e2edee04b81693765bcf780b0f3b5da01a40ddb172942f9ec47"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXGAM-1.0.0-beta+20230315182831.xcframework.zip",
+			checksum: "5f8fedefdda9b7cda26cafda4ce459219295d15abe5c341754044e55f9f0e75c"
 		)
 	]
 )
