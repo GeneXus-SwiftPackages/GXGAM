18dfac64b67f4e29561e133000a77879791b47bd
Build Er <builder@macmini6.local>
2023-06-20 14:05:25 -0300

    Package version: 1.0.0-beta.20230620165651

--- Package.swift
+++ Package.swift
@@ -10,19 +10,19 @@ let package = Package(
 			targets: ["GXGAMWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "1.0.0-beta.20230617144628")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "1.0.0-beta.20230620165651")
 	],
 	targets: [
 		.target(name: "GXGAMWrapper",
 				dependencies: [
 					"GXGAM",
-					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.tvOS, .watchOS, .iOS]))
+					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.tvOS, .iOS, .watchOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXGAM",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXGAM-1.0.0-beta.20230617144628.xcframework.zip",
-			checksum: "aac94c546717d41d1b136964b7b87367b091f5bff3792e6d8b3768597da17dd7"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXGAM-1.0.0-beta.20230620165651.xcframework.zip",
+			checksum: "6c00a1074e693754a852d026a4b4ff4fcb346d014f58321dbd1c716ddbaee85e"
 		)
 	]
 )
