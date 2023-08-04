fcc6f307acfdc965405dcf1cdd458a5364e23af1
genexusbot <builder@genexus.com>
2023-08-04 16:39:47 -0300

    Package version: 1.1.0-beta.9

--- Package.swift
+++ Package.swift
@@ -10,19 +10,19 @@ let package = Package(
 			targets: ["GXGAMWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "1.1.0-beta.8")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "1.1.0-beta.9")
 	],
 	targets: [
 		.target(name: "GXGAMWrapper",
 				dependencies: [
 					"GXGAM",
-					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.tvOS, .watchOS, .iOS]))
+					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.iOS, .tvOS, .watchOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXGAM",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXGAM-1.1.0-beta.8.xcframework.zip",
-			checksum: "643056950201befdc136f2fcecc46558a3252f4035c59f315476a4f6f05fcb01"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXGAM-1.1.0-beta.9.xcframework.zip",
+			checksum: "68b581cb39a19325bc042db15f32ddd2424c2b66e0a6a3a26cdc747a47549415"
 		)
 	]
 )
