b7afc288822b512475ac98fd3559bf3d73a94832
The iOS Team @ GeneXus <builder@genexus.com>
2026-01-14 10:11:51 -0500

    Package version: 4.0.8

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXGAMWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "4.0.7")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "4.0.8")
 	],
 	targets: [
 		.target(name: "GXGAMWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXGAM",
-			url: "https://pkgs.genexus.dev/iOS/releases/GXGAM-4.0.7.xcframework.zip",
-			checksum: "730729fd4eeecc7b201e663291c6b208c1fc78907922ebf6b48a9a18d078a1ba"
+			url: "https://pkgs.genexus.dev/iOS/releases/GXGAM-4.0.8.xcframework.zip",
+			checksum: "7d6682ff6c92c6206d57ceb2808b75640c64a7b9fae4bfd752d008f1962ad438"
 		)
 	]
 )
